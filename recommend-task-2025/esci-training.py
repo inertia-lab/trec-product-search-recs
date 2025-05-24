2bb47de212248b7a6ebe3e8b22c0d92f8b212822
Michael Ekstrand <mdekstrand@drexel.edu>
2025-05-23 21:01:13 -0400

    add training

--- recommend-task-2025/esci-training.py
+++ recommend-task-2025/esci-training.py
@@ -4,3 +4,18 @@ Generate training and validation data from ESCI data.
 Usage:
     esci-training.py
 """
+
+from duckdb import connect, DuckDBPyConnection
+
+ESCI_EXAMPLES = "esci-data/shopping_queries_dataset/shopping_queries_dataset_examples.parquet"
+
+def main():
+    with connect() as db:
+        load_examples(db)
+
+def load_examples(db: DuckDBPyConnection):
+    db.execute(f"CREATE TABLE esci_queries AS select * from '{ESCI_EXAMPLES}'")
+
+
+if __name__ == '__main__':
+    main()
