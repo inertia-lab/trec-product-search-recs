679e9338583006622cb2b47d9043b002691b226f
Michael Ekstrand <mdekstrand@drexel.edu>
2025-06-04 17:17:56 -0400

    update collect-asins

--- recommend-task-2025/collect-ucsd-asins.py
+++ recommend-task-2025/collect-ucsd-asins.py
@@ -1,42 +1,48 @@
 """
 Collect known ASINs from the UCSD Amazon data.
 
+Note: this expects the data to be ZSTD-compressed.
+
 Usage:
-    collect-ucsd-asins.py [-v]
+    collect-ucsd-asins.py [-v] DIR
 
 Options:
     -v, --verbose
         Turn on verbose logging.
+    DIR
+        Path to the directory containing Amazon meatadata.
 """
 
 import logging
 import sys
-from docopt import docopt
-from duckdb import connect, DuckDBPyConnection
 
-log = logging.getLogger('trec-product.collect')
+from docopt import docopt
+from duckdb import DuckDBPyConnection, connect
+
+log = logging.getLogger("trec-product.collect")
+
 
 def main(options):
-    level = logging.DEBUG if options['--verbose'] else logging.INFO
+    level = logging.DEBUG if options["--verbose"] else logging.INFO
     logging.basicConfig(level=level, stream=sys.stderr)
     with connect() as db:
-        db.execute('SET enable_progress_bar = TRUE')
-        collect_asins(db)
+        db.execute("SET enable_progress_bar = TRUE")
+        collect_asins(db, options["DIR"])
 
 
-def collect_asins(db: DuckDBPyConnection):
+def collect_asins(db: DuckDBPyConnection, path: str):
     log.info("copying ASINs")
     db.execute(
-        """
+        f"""
         COPY (
             SELECT DISTINCT parent_asin AS asin
-            FROM 'ucsd-2023/meta_*.jsonl.zst'
+            FROM '{path}/meta_*.jsonl.zst'
             ORDER BY asin
         ) TO 'ucsd-asins.parquet' (COMPRESSION ZSTD)
         """
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     opts = docopt(__doc__ or "")
     main(opts)
